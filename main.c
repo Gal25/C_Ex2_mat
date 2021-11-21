adf8099de722e152c98f903fb29f1036093dec4d
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-21 18:55:36 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include "my_mat.h"
+
+int main(){
+   
+   char i;
+   int [][]ans;
+   scanf("%c",&i);
+
+   if(i == 'A'){
+        A(ans);
+   }
+   if(i == 'B'){
+        B(ans);
+   }
+   if(i == 'C'){
+        C(ans);
+   }
+
+
+
+return 0;
+}
