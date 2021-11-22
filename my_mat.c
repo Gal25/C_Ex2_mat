ca38f04c84d6bc98e359e78077d75225b9dedf52
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-22 18:23:02 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -2,62 +2,68 @@
 #include <math.h>
 #include "my_mat.h"
 
-int A(){
+int mat[10][10];
+
+void A(){
      int i, j;
-     int mat[10][10];
-     for(i=0; i<=10; i++){
-          for(j=0; j <=10; j++){
+     for(i=0; i<10; i++){
+          for(j=0; j <10; j++){
                scanf("%d", &mat[i][j]);
           }
      }
-     return mat;
-
 }
 
-void B(int mat[10][10]){
-    int i,j;
-    // int ans[10][10];
-    scanf("%d %d", &i, &j);
-    // ans = C(mat);
-     if(mat[i][j] >= 0){
-         printf("%s", "True");
-     }
-     else {
-         printf("%s", "False");
-     }
-}
 
-int min(int x, int y);
-void print_ans(int ans[10][10]);
-void C(int mat[10][10]){
-int ans[10][10] , i , j, k;
+
+void B(){
+int i,j, k;
 scanf("%d %d", &i, &j);
-for(k=0; k <= 10; k++){
-    for(i=0; i<=10; i++){
-        for(j=0; j<=10; j++){
-            ans[i][j]= min(ans[i][j],ans[i][k]+ans[k][j]);
-        }
-    }
-}
-
-print_ans(ans);
-}
-
-void print_ans(int ans[10][10]){
-    int i,j;
-    for(i=0; i<10; i++){
-        for(j=0; j<10; j++){
-            if (ans[i][j] == 0){
-                 printf("%d", -1);
-            }
-            else{
-                printf("%d", ans[i][j]);
-            }  
-        }
-    }
+   
+     if(mat[i][j] > 0){
+        printf("%s", "True \n");
+     } 
+    else{
+        printf("%s", "False \n");
+    } 
+     
 }
 
 int min(int x, int y){
     if (x < y){return x;}
     else {return y;}
+}
+
+void calc(int i_c,int j_c)
+{
+    int ans[10][10], i, j, k;
+
+    for (i = 0; i < 10; i++){
+        for (j = 0; j < 10; j++){
+            ans[i][j] = mat[i][j];
+        }
+    }
+    for (k = 0; k < 10; k++) {
+        for (i = 0; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                ans[i][j]= min(ans[i][j],ans[i][k]+ans[k][j]);
+        
+
+
+            }
+        }
+    }
+
+    if(ans[i_c][j_c]>0){
+    printf("%d\n", ans[i_c][j_c]);
+    }else{
+    printf("-1\n");   
+    }
+
+}
+
+
+void C(){
+int i, j;   
+scanf ("%d %d", &i, &j);
+calc(i,j);
 }
