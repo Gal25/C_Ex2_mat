ed136aceb54c3252ea12a53f80cdcecce3b0103b
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-24 13:05:45 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #define MAX_VALUE 100000000
 #include "my_mat.h"
+#define n 10
 
-int mat[10][10];
+int mat[n][n];
 
 void A(){
      int i, j;
-     for(i=0; i<10; i++){
-          for(j=0; j <10; j++){
+     for(i=0; i<n; i++){
+          for(j=0; j <n; j++){
                scanf("%d", &mat[i][j]);
           }
      }
@@ -24,19 +25,19 @@ void B(){
 
     int i,j,k, t,s;
     scanf("%d %d", &i, &j);
-     int ans[10][10];
+     int ans[n][n];
 
-    for (t = 0; t< 10; t++){
-        for (s = 0; s < 10; s++){
+    for (t = 0; t< n; t++){
+        for (s = 0; s < n; s++){
             ans[t][s] = mat[t][s];
             if(t!=s && mat[t][s]==0){
                 ans[t][s]= MAX_VALUE;
             }
         }
     }
-    for (k = 0; k < 10; k++) {
-        for (t = 0; t < 10; t++) {
-            for (s = 0; s < 10; s++) {
+    for (k = 0; k < n; k++) {
+        for (t = 0; t < n; t++) {
+            for (s = 0; s < n; s++) {
                 ans[t][s]= min(ans[t][s],ans[t][k]+ans[k][s]);
         
 
@@ -56,19 +57,19 @@ void B(){
 
 
 void calc(int t,int s){
-    int ans[10][10], i, j, k;
+    int ans[n][n], i, j, k;
 
-    for (i = 0; i < 10; i++){
-        for (j = 0; j < 10; j++){
+    for (i = 0; i < n; i++){
+        for (j = 0; j < n; j++){
             ans[i][j] = mat[i][j];
             if(i!=j && mat[i][j]==0){
                 ans[i][j]= MAX_VALUE;
             }
         }
     }
-    for (k = 0; k < 10; k++) {
-        for (i = 0; i < 10; i++) {
-            for (j = 0; j < 10; j++) {
+    for (k = 0; k < n; k++) {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
                 ans[i][j]= min(ans[i][j],ans[i][k]+ans[k][j]);
         
 
