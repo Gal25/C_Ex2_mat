55aa8f106f65028baa9d565695782adb149faaaa
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-29 10:54:56 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#define MAX_VALUE 100000000
+#define MAX_VALUE 100000000 //Define if there is no route between i j
 #include "my_mat.h"
-#define n 10
+#define N 10 //The size of the rows and columns of the matrix
 
-int mat[n][n];
+int mat[N][N];
 
 void A(){
      int i, j;
-     for(i=0; i<n; i++){
-          for(j=0; j <n; j++){
+     for(i=0; i<N; i++){
+          for(j=0; j <N; j++){
                scanf("%d", &mat[i][j]);
           }
      }
 }
 
-
+//return the min value
 int min(int x, int y){
     if (x < y){return x;}
     else {return y;}
@@ -25,19 +25,19 @@ void B(){
 
     int i,j,k, t,s;
     scanf("%d %d", &i, &j);
-     int ans[n][n];
+     int ans[N][N];
 
-    for (t = 0; t< n; t++){
-        for (s = 0; s < n; s++){
+    for (t = 0; t< N; t++){
+        for (s = 0; s < N; s++){
             ans[t][s] = mat[t][s];
             if(t!=s && mat[t][s]==0){
                 ans[t][s]= MAX_VALUE;
             }
         }
     }
-    for (k = 0; k < n; k++) {
-        for (t = 0; t < n; t++) {
-            for (s = 0; s < n; s++) {
+    for (k = 0; k < N; k++) {
+        for (t = 0; t < N; t++) {
+            for (s = 0; s < N; s++) {
                 ans[t][s]= min(ans[t][s],ans[t][k]+ans[k][s]);
         
 
@@ -57,19 +57,19 @@ void B(){
 
 
 void calc(int t,int s){
-    int ans[n][n], i, j, k;
+    int ans[N][N], i, j, k;
 
-    for (i = 0; i < n; i++){
-        for (j = 0; j < n; j++){
+    for (i = 0; i < N; i++){
+        for (j = 0; j < N; j++){
             ans[i][j] = mat[i][j];
             if(i!=j && mat[i][j]==0){
                 ans[i][j]= MAX_VALUE;
             }
         }
     }
-    for (k = 0; k < n; k++) {
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
+    for (k = 0; k < N; k++) {
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
                 ans[i][j]= min(ans[i][j],ans[i][k]+ans[k][j]);
         
 
@@ -92,3 +92,4 @@ int i, j;
 scanf ("%d %d", &i, &j);
 calc(i,j);
 }
+
