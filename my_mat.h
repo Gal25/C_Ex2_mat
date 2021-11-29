b5817d5ae9519a17a02a606e36adc47606723ad4
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-29 10:54:23 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,6 +1,6 @@
 
-void A();
-int min(int x, int y);
-void B();
-void calc(int t, int s);
-void C();
+void A(); //Absorb the matrix values from the user
+int min(int x, int y); //return the min value
+void B(); //If there is a route between i and j prints "true" if there is no "false"
+void calc(int t, int s); //Prints the shortest route between i and j and if there is no route then -1
+void C(); //Absorbs the values i and j
